d94177874089bd4e072293ac8194a2b14dacbc34
jagandeep <nice.jagan@gmail.com>
2021-06-01 20:00:45 +0530

    added setup method to create initialization code for fraction add test

--- src/test/java/org/fraction/arithmetic/add/FractionTest.java
+++ src/test/java/org/fraction/arithmetic/add/FractionTest.java
@@ -3,28 +3,30 @@ package org.fraction.arithmetic.add;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for Fraction Test
  */
 public class FractionTest{
-
+    private Fraction fraction;
+    @Before
+    public void setup() {
+        fraction = new Fraction();
+    }
     @Test
     public void shouldAddTwoZeroInteger() {
-        Fraction fraction = new Fraction();
         assertEquals(0, fraction.add(0,0));
     }
 
     @Test
     public void shouldAddZeroAndAnInteger() {
-        Fraction fraction = new Fraction();
         assertEquals(1, fraction.add(0,1));
     }
 
     @Test
     public void shouldAddAnIntegerAndAnInteger() {
-        Fraction fraction = new Fraction();
         assertEquals(2
                 , fraction.add(1,1));
     }
