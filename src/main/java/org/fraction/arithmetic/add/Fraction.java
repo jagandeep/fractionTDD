0723d2109b046b50f6f139ba467b743fcd76709c
jagandeep <nice.jagan@gmail.com>
2021-06-01 19:49:09 +0530

    changed package to add for fraction add functionality

--- src/main/java/org/fraction/arithmetic/add/Fraction.java
+++ src/main/java/org/fraction/arithmetic/add/Fraction.java
@@ -1,4 +1,4 @@
-package org.fraction.arithmetic;
+package org.fraction.arithmetic.add;
 
 /**
  * Hello world!
--- src/test/java/org/fraction/arithmetic/add/FractionTest.java
+++ src/test/java/org/fraction/arithmetic/add/FractionTest.java
@@ -1,4 +1,4 @@
-package org.fraction.arithmetic;
+package org.fraction.arithmetic.add;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
