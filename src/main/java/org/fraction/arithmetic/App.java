e68d0d2db89c7a143839990b41521e18c93fc786
jagandeep <nice.jagan@gmail.com>
2021-06-01 19:27:46 +0530

    created repository and initial code setup witha failing test

---
+++ src/main/java/org/fraction/arithmetic/App.java
@@ -0,0 +1,13 @@
+package org.fraction.arithmetic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
